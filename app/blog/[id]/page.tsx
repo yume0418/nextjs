b663ac67471549c1fcd68f1336346c2f3d944097
yumesato <crazydoll04189@gmail.com>
2025-04-01 00:55:02 +0900

    追加分

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -3,7 +3,9 @@ import BlogPostDetail from '@/app/ui/BlogPostDetail';
 import CommentSection from '@/app/ui/CommentSection';
 import RelatedPosts from '@/app/ui/RelatedPosts';
 import { notFound } from 'next/navigation';
-import Link from 'next/link'
+import Link from 'next/link';
+
+const siteUrl = process.env.NEXT_PUBLIC_SITE_URL || 'http://localhost:3000';
 
 async function getPost(id: string): Promise<Post | null> {
   const res = await fetch(`${siteUrl}/api/posts/${id}`, { cache: 'no-store' });
@@ -18,7 +20,7 @@ async function getPost(id: string): Promise<Post | null> {
 }
 
 async function getComments(postId: string): Promise<Comment[]> {
-  const res = await fetch(`http://localhost:3000/api/comments?postId=${postId}`, { cache: 'no-store' });
+  const res = await fetch(`${siteUrl}/api/comments?postId=${postId}`, { cache: 'no-store' });
   if (!res.ok) {
     console.error(`Failed to fetch comments for postId ${postId}`);
     throw new Error('Failed to fetch comments');
@@ -29,7 +31,7 @@ async function getComments(postId: string): Promise<Comment[]> {
 
 async function getRelatedPosts(postId: string): Promise<Post[]> {
   try {
-    const res = await fetch(`http://localhost:3000/api/posts/${postId}/related`, { cache: 'no-store' });
+    const res = await fetch(`${siteUrl}/api/posts/${postId}/related`, { cache: 'no-store' });
     if (!res.ok) {
       if (res.status === 404) {
         console.log(`No related posts found for postId ${postId}`);
@@ -63,8 +65,9 @@ export default async function BlogPost({ params }: { params: { id: string } }) {
       <CommentSection postId={post.id} initialComments={comments} />
       <Link
         href={`/blog/edit/${post.id}`} 
-        className="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white !bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 disabled:opacity-50"
-      >編集
+        className="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 disabled:opacity-50"
+      >
+        編集
       </Link>
     </div>
   );
